59130497cd8bc8661ca9daa2e0e837dd1ab15b67
Yannick Nana <nanayannick1998@gmail.com>
2023-05-09 21:33:30 +0200

    feat: Add projects

--- _data/sites/projects.js
+++ _data/sites/projects.js
@@ -1,5 +1,5 @@
 module.exports = {
-	description: "Front-end Testing Tools",
+	description: "Personal projects",
 	// skip if localhost
 	// skip if this is a new fork of the speedlify (not Zach’s)
 	skip: !process.env.CONTEXT || process.env.SITE_NAME !== "speedlify",
@@ -9,18 +9,11 @@ module.exports = {
 		//           and we can reset chrome with each run instead of
 		//           each site in every run (it’s faster)
 		// Use "site" if sites are all on the same origin and share assets.
-		freshChrome: "site",
+		freshChrome: "run",
 	},
 	urls: [
-		"https://eslint.org/",
-		"https://qunitjs.com/",
-		"https://karma-runner.github.io/latest/index.html",
-		"https://gulpjs.com/",
-		"https://webhint.io/",
-		"https://gruntjs.com/",
-		"https://theintern.io/",
-		"https://istanbul.js.org/",
-		"https://webdriver.io/",
-		"https://mochajs.org/"
+		"https://frontendstories.com/",
+		"https://linkedin-embed.netlify.app/",
+		"https://terribleprojectideas.com/"
 	]
 };
